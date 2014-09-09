97d158050b98f16cec38d3aa8b025145d5e6812b
Marco de Jongh <marco.dejongh@gfk.com>
2014-09-09 17:41:20 +0200

    Goodbye meteorite and velocity

--- run_nightwatch.sh
+++ run_nightwatch.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-echo "installing nightwatch in .meteor/local/build"
-  mkdir -p .meteor/local/build
-  cd .meteor/local/build
-  npm install nightwatch@0.5.3
-  cd ../../../
+npm install
 
 sed -i.bak "s/#USERNAME#/${BROWSERSTACK_USERNAME}/g" nightwatch.json
 sed -i.bak "s/#KEY#/${BROWSERSTACK_KEY}/g" nightwatch.json
@@ -11,11 +7,11 @@ sed -i.bak "s/#KEY#/${BROWSERSTACK_KEY}/g" nightwatch.json
 sed -i.bak "s/#EMAIL#/${METEOR_EMAIL}/g" deploy.exp
 sed -i.bak "s/#PASSWORD#/${METEOR_PASSWORD}/g" deploy.exp
 sed -i.bak "s/#SITENAME#/${METEOR_DEPLOY_URL}/g" deploy.exp
-
-expect deploy.exp
-
+cd app
+expect ../deploy.exp
+cd ..
 echo "Waiting 10 seconds for meteor to finish deploying"
 sleep 10
 
-echo "running nightwatch from app root"
-   .meteor/local/build/node_modules/nightwatch/bin/nightwatch -c nightwatch.json $1 $2
+echo "running nightwatch from node_modules"
+   node_modules/nightwatch/bin/nightwatch -c nightwatch.json $1 $2
