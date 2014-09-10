81a12c50c69ec31b7facd05429a8d1b8d6618d0d
Marco de Jongh <marco.dejongh@gfk.com>
2014-09-10 15:33:13 +0200

    More cleanup

--- run_nightwatch.sh
+++ run_nightwatch.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-cd app
-expect ../deploy.exp
-cd ..
-
-echo "Waiting 10 seconds for meteor to finish deploying"
-sleep 10
-
 echo "running nightwatch from node_modules"
    node_modules/nightwatch/bin/nightwatch -c nightwatch.json $1 $2
